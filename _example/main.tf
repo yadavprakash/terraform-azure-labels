b3bb4e2448c36b55c749fe02f57687623e9ea978
sohan yadav <sohanlalyadav88@gmail.com>
2024-02-13 10:09:24 +0530

    initial commit

---
+++ _example/main.tf
@@ -0,0 +1,16 @@
+provider "azurerm" {
+  features {}
+}
+
+module "labels" {
+  source = "./../"
+
+  name          = "labels"
+  environment   = "test"
+  label_order   = ["name", "environment"]
+  business_unit = "Corp"
+  attributes    = ["private"]
+  extra_tags = {
+    Application = "test"
+  }
+}
